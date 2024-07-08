0baf7bbebb79ba852e9f7a4d7d44a2db51250a73
Stuart Turner <stuart@turner-isageek.com>
2024-07-08 14:22:02 -0500

    Fix build issues

--- Web/Program.cs
+++ Web/Program.cs
@@ -4,7 +4,6 @@ using DryIoc;
 using DryIoc.Microsoft.DependencyInjection;
 using Hangfire;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Options;
 using Serilog;
 using VsaTemplate.Web;
 using VsaTemplate.Web.Components;
